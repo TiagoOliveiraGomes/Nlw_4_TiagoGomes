4bb298f824c78e993fed6ac69c2cad0f126a2543
TiagoOliveiraGomes1 <tiago.oliveira@teclat.com.br>
2022-05-07 20:17:42 -0300

    first commit

---
+++ mobile/src/components/options/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { feedbackTypes } from '../../utils/feedbackTypes';
+
+import { Copyright } from '../copyright';
+import { Option } from '../option';
+import { FeedbackType } from '../widget';
+
+import { styles } from './styles';
+
+interface Props {
+  onFeedbackTypeChanged: (feedbackType: FeedbackType) => void;
+}
+
+export function Options({ onFeedbackTypeChanged }: Props) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Deixe seu feedback</Text>
+
+      <View style={styles.options}>
+        {Object.entries(feedbackTypes).map(([key, value]) => (
+          <Option
+            key={key}
+            title={value.title}
+            image={value.image}
+            onPress={() => onFeedbackTypeChanged(key as FeedbackType)}
+          />
+        ))}
+      </View>
+
+      <Copyright />
+    </View>
+  );
+}
