ef480a2e480f83ab406944e649941c728390de38
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-05-11 15:47:30 -0300

    expo finishes

--- mobile/src/libs/api.ts
+++ mobile/src/libs/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: "http://localhost:3333"
+    baseURL: "https://nlw1servertiagogomes-production.up.railway.app"
 })
+
+//url: 192.168.0.106
